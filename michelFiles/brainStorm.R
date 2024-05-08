1e7f8230dcbac0da1b1c75ba63f9f81aff7b0b5a
Michel <michelcyc@gmail.com>
2024-05-08 13:31:11 -0300

    commit

--- michelFiles/brainStorm.R
+++ michelFiles/brainStorm.R
@@ -1,1 +1,18 @@
-load(url("https://github.com/cefet-rj-dal/softed/blob/main/experiment_code/datasets_evts_ref.RData?raw=true"))
+#Install package
+install.packages("devtools")
+library(devtools)
+devtools::install_github("cefet-rj-dal/event_datasets", force=TRUE)
+library(dalevents)
+
+#Load a series
+data(eia_oil_prices)
+series <- eia_oil_prices$
+
+#Use the loaded series
+summary(series)
+plot(series$series, type = "l")
+#or
+plot(series$value, type = "l")
+
+#Get information about a series
+?yahoo_a1
