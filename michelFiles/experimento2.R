8f47784fe7b69ea2daa07cf7ab541cb9730fdca2
Michel <michelcyc@gmail.com>
2024-07-05 21:33:41 -0300

    c

--- michelFiles/experimento2.R
+++ michelFiles/experimento2.R
@@ -12,17 +12,21 @@ source("michelFiles/my_utils.R")
 
 dataset <- read_csv('michelFiles/meus_dados.csv')
 
-indexDaSerie <- 1:length(dataset$PreprocessedSeries)
-plot_ts(x=indexDaSerie, y=dataset$PreprocessedSeries)
+#indexDaSerie <- 1:length(dataset$PreprocessedSeries)
+#plot_ts(x=indexDaSerie, y=dataset$PreprocessedSeries)
 
 har_model <- hanr_arima()
 har_fitted_model <- fit(har_model, dataset$PreprocessedSeries)
 fitted_detection_model <- detect(obj = har_fitted_model, dataset$PreprocessedSeries)
-grf <- har_plot(har_fitted_model, dataset$PreprocessedSeries, fitted_detection_model, dataset$Classe)
-plot(grf)
+#grf <- har_plot(har_fitted_model, dataset$PreprocessedSeries, fitted_detection_model, dataset$Classe)
+#plot(grf)
 
 newSoftEval <- evaluate(har_eval_soft(sw_size=10), fitted_detection_model$event, dataset$Classe)
 printEval(newSoftEval)
 
 hardEval <- evaluate(har_eval(), fitted_detection_model$event, dataset$Classe)
 printEval(hardEval)
+
+execution_time <- system.time({
+  hardEval <- evaluate(har_eval(), fitted_detection_model$event, dataset$Classe)
+})
