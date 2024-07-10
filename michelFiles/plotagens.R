2bb34e12a12e8cdc7db8f711ad4cc1175cd62b56
Michel <michelcyc@gmail.com>
2024-07-10 19:18:45 -0300

    c

--- michelFiles/plotagens.R
+++ michelFiles/plotagens.R
@@ -1,4 +1,3 @@
-# Load necessary packages
 library(ggplot2)
 library(dplyr)
 library(tidyr)
@@ -15,7 +14,7 @@ mean_time_soft2 <- mean(unlist(soft2$time))
 # NORMALIZING
 combined_time <- c(unlist(hard$time), unlist(soft1$time), unlist(soft2$time))
 
-# Step 2: Create a normalization function based on the combined time data
+# Create a normalization function based on the combined time data
 min_time <- min(combined_time)
 max_time <- max(combined_time)
 normalize_time <- function(x) {
@@ -44,26 +43,30 @@ soft2 <- list(
 # Create a combined data frame
 data <- bind_rows(
   as_tibble(hard) %>% mutate(Method = "Hard"),
-  as_tibble(soft1) %>% mutate(Method = "Soft 1"),
-  as_tibble(soft2) %>% mutate(Method = "Soft 2")
+  as_tibble(soft1) %>% mutate(Method = "Soft Trad"),
+  as_tibble(soft2) %>% mutate(Method = "Soft Par")
 ) %>% pivot_longer(-Method, names_to = "Metric", values_to = "Value")
 
-# Plot setup with no legends
+data$Method <- factor(data$Method, levels = c("Hard", "Soft Trad", "Soft Par"))
+
+# Plot setup with legend at the bottom
 ggplot(data, aes(x = Metric, y = Value, fill = Method)) +
   geom_boxplot() +
   labs(title = NULL,
        x = NULL, y = NULL) +
   theme_minimal() +
-  theme(axis.text.x = element_text(hjust = 1, size = 14),  # Adjust text angle for better readability
-        legend.position = "right",
+  theme(axis.text.x = element_text(hjust = 1, size = 14),  # Adjust text angle and size for better readability
+        legend.position = "bottom",
         legend.title = element_text(size = 14, face = "bold"),  # Increase and bold the legend title font size
-        legend.text = element_text(size = 12),  # Increase the legend text font size
+        legend.text = element_text(size = 14),  # Increase the legend text font size
         legend.key.size = unit(1.5, "lines")) +  # Increase the size of the legend keys
   scale_fill_brewer(palette = "Pastel1") +
-  guides(fill = guide_legend(title = "Método"))
+  guides(fill = guide_legend(title = "Metric"))
 
 # Save the plot
-ggsave("metric_distribution.png", width = 12, height = 8, units = "in")
+ggsave("metric_distribution.png", plot = p, width = 12, height = 8, units = "in")
+
+
 
 
 #   Plotagem 2
@@ -77,27 +80,35 @@ library(reshape2)
 
 # Dados
 data <- data.frame(
-  Metrica = c("acurácia", "precisão", "revocação", "F1"),
-  Metrica_dura = c(0.96, 0.33, 0.33, 0.33),
-  SoftED_original = c(0.97, 0.63, 0.63, 0.63),
-  SoftED_expandido = c(0.98, 0.76, 0.76, 0.76)
+  Metric = c("accuracy", "precision", "recall", "F1"),
+  Hard = c(0.96, 0.33, 0.33, 0.33),
+  Softed_Trad = c(0.97, 0.63, 0.63, 0.63),
+  Softed_Par = c(0.98, 0.76, 0.76, 0.76)
 )
 
+# Remover underscores dos nomes das colunas
+colnames(data) <- gsub("_", " ", colnames(data))
+
 # Converter dados para o formato longo
-data_melted <- melt(data, id.vars = "Metrica")
+data_melted <- melt(data, id.vars = "Metric")
+data_melted$Metric <- factor(data_melted$Metric, levels = c("accuracy", "precision", "recall", "F1"))
+
 
 # Plot
-ggplot(data_melted, aes(x = Metrica, y = value, fill = variable)) +
-  geom_bar(stat = "identity", position = "dodge") +
+ggplot(data_melted, aes(x = Metric, y = value, fill = variable)) +
+  geom_bar(stat = "identity", position = "dodge", color = "black") +  # Add black border to bars
   geom_text(aes(label = value), vjust = 1.6, color = "white", position = position_dodge(0.9), size = 4, fontface = "bold") +
-  labs(fill = "Método") +
+  labs(fill = "Metric") +
   theme_minimal() +
   theme(
-    text = element_text(size = 15),  # Aumentar tamanho do texto
-    axis.title.x = element_blank(),  # Remover título do eixo x
-    axis.title.y = element_blank(),  # Remover título do eixo y
-    axis.text = element_text(size = 12),  # Aumentar tamanho do texto dos eixos
-    legend.title = element_text(size = 15),  # Aumentar tamanho do título da legenda
-    legend.text = element_text(size = 12)  # Aumentar tamanho do texto da legenda
+    axis.title.x = element_blank(),  # Remove x-axis title
+    axis.text.x = element_text(size = 14),
+    axis.text.y = element_blank(),  # Remove y-axis text
+    axis.title.y = element_blank(),  # Remove y-axis title
+    legend.position = "bottom",
+    legend.title = element_text(size = 14, face = "bold"),  # Increase and bold the legend title font size
+    legend.text = element_text(size = 14),  # Increase the legend text font size
+    legend.key.size = unit(1.5, "lines")
   ) +
-  scale_fill_manual(values = c("Metrica_dura" = "lightblue", "SoftED_original" = "lightcoral", "SoftED_expandido" = "lightgreen"))  # Cores pastel
+  scale_fill_brewer(palette = "Pastel1")
+
