a74e5a12a0fa9295c10054a78c1f1b8ae3ac1f80
Michel Siqueira Reis <michelcyc@gmail.com>
2025-05-09 04:26:35 -0300

    x

--- R/har_eval_soft.R
+++ R/har_eval_soft.R
@@ -133,11 +133,11 @@ evaluate.har_eval_soft <- function(obj, detection, event, ...) {
   m <- length(which(event))
   t <- length(event)
 
-  #scores <- scores / obj$sw_size
-  TPs <- sum(scores)
-  FPs <- sum(1-scores)
-  FNs <- m-TPs
-  TNs <- (t-m)-FPs
+  ajuste <- obj$sw_size/4
+  TPs <- (sum(scores))/ajuste
+  FPs <- (sum(1-scores))*ajuste
+  FNs <- (m-sum(scores))*ajuste
+  TNs <- (t-m)-(sum(1-scores))/ajuste
 
   confMatrix <- as.table(matrix(c(as.character(TRUE),as.character(FALSE),
                                   round(TPs,2),round(FPs,2),
