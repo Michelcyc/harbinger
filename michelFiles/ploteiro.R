1d8eb47af11a5536459d2b395642409425f46d5e
Michel Siqueira Reis <michelcyc@gmail.com>
2025-11-11 14:30:58 -0300

    x

--- michelFiles/ploteiro.R
+++ michelFiles/ploteiro.R
@@ -217,38 +217,3 @@ ggplot(df_media_eventos, aes(x = reorder(dataset, media_eventos),
     panel.grid.major.y = element_blank(),
     axis.text.y = element_text(size = 11)
   )
-
-
-######################################################
-########## REGISTRO DE TEMPO DE EXECUCAO #############
-######################################################
-
-suppressPackageStartupMessages({
-  library(dplyr)
-})
-
-# Rótulo deste “run” para compor o nome do arquivo
-run_label <- "run_softed"
-#run_label <- "run_softedpar"
-#run_label <- "run_smartsofted"
-
-time_by_dataset <- resumo_experimentos %>%
-  group_by(dataset) %>%
-  summarise(
-    total_time_metric = sum(time_metric, na.rm = TRUE),
-    n_series          = n(),
-    .groups = "drop"
-  ) %>%
-  arrange(desc(total_time_metric))
-
-# Cria pasta e salva o objeto para uso posterior
-dir.create("results", showWarnings = FALSE, recursive = TRUE)
-save(
-  time_by_dataset,
-  file = file.path("results", paste0("time_by_dataset_", run_label, ".RData")),
-  compress = "xz"
-)
-
-# (opcional) imprimir um preview
-print(time_by_dataset, n = Inf)
-
